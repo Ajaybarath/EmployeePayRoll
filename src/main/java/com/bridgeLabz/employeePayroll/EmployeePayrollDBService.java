c4fc4a50d4747146bc2e9e3b774613598352cc58
ajaybarath <ajaybarath3@gmail.com>
2021-09-26 18:41:41 +0530

    ADDED: function to connect to database and get the data

--- src/main/java/com/bridgeLabz/employeePayroll/EmployeePayrollDBService.java
+++ src/main/java/com/bridgeLabz/employeePayroll/EmployeePayrollDBService.java
@@ -1,2 +1,42 @@
-package com.bridgeLabz.employeePayroll;public class EmployeePayrollDBService {
+package com.bridgeLabz.employeePayroll;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollDBService {
+
+    private Connection getConnection() throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "ajaybarath1999";
+        Connection connection;
+
+       System.out.println("Connecting to database : " + jdbcURL);
+       connection = DriverManager.getConnection(jdbcURL, userName, password);
+       System.out.println("Connection is successfull : " + connection);
+       return connection;
+
+    }
+
+    public List<EmployeePayRollData> readData() throws SQLException {
+
+        String sql = "Select * from employee";
+        List<EmployeePayRollData> employeePayRollDataList = new ArrayList<>();
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            double salary = resultSet.getDouble("salary");
+            LocalDate date = resultSet.getDate("start").toLocalDate();
+            employeePayRollDataList.add(new EmployeePayRollData(id, name, salary, date));
+
+        }
+
+        return employeePayRollDataList;
+    }
 }
