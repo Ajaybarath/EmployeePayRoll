3424f5b12628d8bfce9820bfd160cf72d7493f7c
ajaybarath <ajaybarath3@gmail.com>
2021-09-26 18:42:28 +0530

    ADDED: function to test reading the database

--- src/test/java/NIOFileApiTest.java
+++ src/test/java/NIOFileApiTest.java
@@ -1,13 +1,15 @@
-package com.bridgeLabz.employeePayroll;
-
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+import com.bridgeLabz.employeePayroll.EmployeePayRollData;
+import com.bridgeLabz.employeePayroll.EmployeePayRollService;
+import com.bridgeLabz.employeePayroll.JavaWatch8ServiceExample;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,37 +19,37 @@ public class NIOFileApiTest {
 	private static final String HOME = System.getProperty("user.home");
 	private static final String PLAY_WITH_NIO = "TempFileTry";
 
-	@Test
-	public void givenPathWhenCheckedThenConfirm() throws IOException {
-		
-		Path homepath = Paths.get(HOME);
-		
-		Assert.assertTrue(Files.exists(homepath));
-		
-		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-		if (Files.exists(playPath)) Files.deleteIfExists(playPath);
-		Assert.assertTrue(Files.notExists(playPath));
-		
-		IntStream.range(1, 10).forEach(cntr -> {
-			Path tempFile = Paths.get(playPath + "/temp" + cntr);
-			Assert.assertTrue(Files.notExists(tempFile));
-			try {
-				Files.createFile(tempFile);
-			}
-			catch (IOException e) {
-				Assert.assertTrue(Files.exists(tempFile));
-			}
-		});
-		
-	}
+//	@Test
+//	public void givenPathWhenCheckedThenConfirm() throws IOException {
+//
+//		Path homepath = Paths.get(HOME);
+//
+//		Assert.assertTrue(Files.exists(homepath));
+//
+//		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+//		if (Files.exists(playPath)) Files.deleteIfExists(playPath);
+//		Assert.assertTrue(Files.notExists(playPath));
+//
+//		IntStream.range(1, 10).forEach(cntr -> {
+//			Path tempFile = Paths.get(playPath + "/temp" + cntr);
+//			Assert.assertTrue(Files.notExists(tempFile));
+//			try {
+//				Files.createFile(tempFile);
+//			}
+//			catch (IOException e) {
+//				Assert.assertTrue(Files.exists(tempFile));
+//			}
+//		});
+//
+//	}
 	
 	
-	@Test
-	public void givenADirectoryWhenWatchedListAllTheActivities() throws IOException {
-		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
-		new JavaWatch8ServiceExample(dir).processEvents(); 
-	}
+//	@Test
+//	public void givenADirectoryWhenWatchedListAllTheActivities() throws IOException {
+//		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+//		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+//		new JavaWatch8ServiceExample(dir).processEvents();
+//	}
 	
 	@Test
 	public void writeDataAndGetEntriesCount() {
@@ -89,10 +91,24 @@ public class NIOFileApiTest {
 		};
 		
 		EmployeePayRollService employeePayRollService = new EmployeePayRollService(Arrays.asList(arrEmployeePayRollDatas));
+		employeePayRollService.writeEmployeePayrollData(EmployeePayRollService.IOService.FILE_IO);
 		employeePayRollService.readEmployeePayrollData();
 		long entries = employeePayRollService.countEntries();
 		Assert.assertEquals(3, entries);
 	
 	}
+
+	@Test
+	public void readDatabaseAndGetEntriesCount() throws IOException, SQLException {
+
+		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
+		employeePayRollService.readEmployeePayrollData(EmployeePayRollService.IOService.DB_IO);
+		long entries = employeePayRollService.listSize();
+		employeePayRollService.printList();
+		Assert.assertEquals(6, entries);
+
+	}
+
+
 
 }
