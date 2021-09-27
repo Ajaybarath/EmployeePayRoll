56a30c7546e9de9ab74fbeec4f64a1445bae3b8b
ajaybarath <ajaybarath3@gmail.com>
2021-09-27 16:34:14 +0530

    ADDED: function to get the payroll data by name and made into singleton class

--- src/main/java/com/bridgeLabz/employeePayroll/EmployeePayrollDBService.java
+++ src/main/java/com/bridgeLabz/employeePayroll/EmployeePayrollDBService.java
@@ -7,6 +7,19 @@ import java.util.List;
 
 public class EmployeePayrollDBService {
 
+    PreparedStatement employeePayrollDataService;
+    private static EmployeePayrollDBService employeePayrollDBService;
+
+    private EmployeePayrollDBService() {
+    }
+
+    public static EmployeePayrollDBService getInstance() {
+        if (employeePayrollDBService == null)
+            employeePayrollDBService = new EmployeePayrollDBService();
+
+        return employeePayrollDBService;
+    }
+
     private Connection getConnection() throws SQLException {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
         String userName = "root";
@@ -30,14 +43,7 @@ public class EmployeePayrollDBService {
             Statement statement = null;
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                double salary = resultSet.getDouble("salary");
-                LocalDate date = resultSet.getDate("start").toLocalDate();
-                employeePayRollDataList.add(new EmployeePayRollData(id, name, salary, date));
-
-            }
+            employeePayRollDataList = this.getEmployeePayrollData(resultSet);
 
             return employeePayRollDataList;
         } catch (SQLException throwables) {
@@ -57,4 +63,49 @@ public class EmployeePayrollDBService {
             throw new EmployeePayrollException("Check db connectivity");
         }
     }
-}
+
+    public List<EmployeePayRollData> getEmployeePayrollData(String name) throws EmployeePayrollException {
+        List<EmployeePayRollData> employeePayRollDataList = null;
+        if (this.employeePayrollDataService == null) {
+            this.prepareStatementForEmployeeData();
+        }
+
+        try {
+            employeePayrollDataService.setString(1, name);
+            ResultSet resultSet = employeePayrollDataService.executeQuery();
+            employeePayRollDataList = this.getEmployeePayrollData(resultSet);
+        } catch (SQLException throwables) {
+            throw new EmployeePayrollException(throwables.getMessage());
+        }
+
+        return employeePayRollDataList;
+    }
+
+    private List<EmployeePayRollData> getEmployeePayrollData(ResultSet resultSet) throws EmployeePayrollException {
+        List<EmployeePayRollData> employeePayRollDataList = new ArrayList<>();
+
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+                LocalDate date = resultSet.getDate("start").toLocalDate();
+                employeePayRollDataList.add(new EmployeePayRollData(id, name, salary, date));
+            }
+        } catch (SQLException throwables) {
+            throw new EmployeePayrollException(throwables.getMessage());
+        }
+        return employeePayRollDataList;
+    }
+
+    private void prepareStatementForEmployeeData() throws EmployeePayrollException {
+        try {
+            Connection connection = this.getConnection();
+            String sql = "select * from employee where name = ?";
+            employeePayrollDataService = connection.prepareStatement(sql);
+
+        } catch (SQLException throwables) {
+            throw new EmployeePayrollException(throwables.getMessage());
+        }
+    }
+ }
