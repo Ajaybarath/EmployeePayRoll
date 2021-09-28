5e43d6b94104fe86c1d43a9207e0a0b7d671428b
ajaybarath <ajaybarath3@gmail.com>
2021-09-28 23:14:27 +0530

    Removed : the dbService test cases

--- src/test/java/NIOFileApiTest.java
+++ src/test/java/NIOFileApiTest.java
@@ -21,37 +21,36 @@ public class NIOFileApiTest {
 	private static final String HOME = System.getProperty("user.home");
 	private static final String PLAY_WITH_NIO = "TempFileTry";
 
-//	@Test
-//	public void givenPathWhenCheckedThenConfirm() throws IOException {
-//
-//		Path homepath = Paths.get(HOME);
-//
-//		Assert.assertTrue(Files.exists(homepath));
-//
-//		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-//		if (Files.exists(playPath)) Files.deleteIfExists(playPath);
-//		Assert.assertTrue(Files.notExists(playPath));
-//
-//		IntStream.range(1, 10).forEach(cntr -> {
-//			Path tempFile = Paths.get(playPath + "/temp" + cntr);
-//			Assert.assertTrue(Files.notExists(tempFile));
-//			try {
-//				Files.createFile(tempFile);
-//			}
-//			catch (IOException e) {
-//				Assert.assertTrue(Files.exists(tempFile));
-//			}
-//		});
-//
-//	}
+	@Test
+	public void givenPathWhenCheckedThenConfirm() throws IOException {
+
+		Path homepath = Paths.get(HOME);
+
+		Assert.assertTrue(Files.exists(homepath));
+
+		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		if (Files.exists(playPath)) Files.deleteIfExists(playPath);
+		Assert.assertTrue(Files.notExists(playPath));
+
+		IntStream.range(1, 10).forEach(cntr -> {
+			Path tempFile = Paths.get(playPath + "/temp" + cntr);
+			Assert.assertTrue(Files.notExists(tempFile));
+			try {
+				Files.createFile(tempFile);
+			}
+			catch (IOException e) {
+				Assert.assertTrue(Files.exists(tempFile));
+			}
+		});
+
+	}
 	
 	
-//	@Test
-//	public void givenADirectoryWhenWatchedListAllTheActivities() throws IOException {
-//		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-//		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
-//		new JavaWatch8ServiceExample(dir).processEvents();
-//	}
+	@Test
+	public void givenADirectoryWhenWatchedListAllTheActivities() throws IOException {
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+	}
 	
 	@Test
 	public void writeDataAndGetEntriesCount() {
@@ -100,108 +99,5 @@ public class NIOFileApiTest {
 	
 	}
 
-	@Test
-	public void readDatabaseAndGetEntriesCount() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		employeePayRollService.readEmployeePayrollData(EmployeePayRollService.IOService.DB_IO);
-		long entries = employeePayRollService.listSize();
-		employeePayRollService.printList();
-		Assert.assertEquals(6, entries);
-
-	}
-
-	@Test
-	public void readDatabaseAndUpdateTheDatabase() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		employeePayRollService.readEmployeePayrollData(EmployeePayRollService.IOService.DB_IO);
-		employeePayRollService.updateEmployeeData("Tresia", 2000);
-
-	}
-
-	@Test
-	public void getEmployeePayrollDataByName() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		List<EmployeePayRollData> employeePayRollDataList = employeePayRollService.getEmployeePayrollDataByName("Tresia");
-
-		Assert.assertEquals("Tresia", employeePayRollDataList.get(0).getName());
-
-	}
-
-	@Test
-	public void getEmployeePayrollDataByDate() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		List<EmployeePayRollData> employeePayRollDataList = employeePayRollService.getEmployeePayrollDataByDateRange("2021-03-01", "2021-12-01");
-		for (EmployeePayRollData data : employeePayRollDataList){
-			System.out.println(data.getName());
-		}
-		Assert.assertEquals("Tresia", employeePayRollDataList.get(0).getName());
-
-	}
-
-	@Test
-	public void getSumOfSalaryOfEmployee() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		List<EmployeePayRollData> employeePayRollDataList = employeePayRollService.getSumOfEmployeeSalary();
-		for (EmployeePayRollData data : employeePayRollDataList){
-			System.out.println(data.getGender() + ", " + data.getSalary());
-		}
-		Assert.assertEquals(160000.0, employeePayRollDataList.get(0).getSalary(), 0.00);
-
-	}
-
-	@Test
-	public void getAvgSalaryOfEmployee() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-
-		List<EmployeePayRollData> employeeAvgSalaryList = employeePayRollService.getAvgSumMinMaxEmployeeSalary("avg");
-		List<EmployeePayRollData> employeeMinSalaryList = employeePayRollService.getAvgSumMinMaxEmployeeSalary("min");
-		List<EmployeePayRollData> employeeMaxSalaryList = employeePayRollService.getAvgSumMinMaxEmployeeSalary("max");
-
-
-		for (EmployeePayRollData data : employeeAvgSalaryList){
-			System.out.println(data.getGender() + ", " + data.getSalary());
-		}
-		for (EmployeePayRollData data : employeeMinSalaryList){
-			System.out.println(data.getGender() + ", " + data.getSalary());
-		}
-		for (EmployeePayRollData data : employeeMaxSalaryList){
-			System.out.println(data.getGender() + ", " + data.getSalary());
-		}
-		Assert.assertEquals(40000.0, employeeAvgSalaryList.get(0).getSalary(), 0.00);
-		Assert.assertEquals(30000.0, employeeMinSalaryList.get(0).getSalary(), 0.00);
-		Assert.assertEquals(50000.0, employeeMaxSalaryList.get(0).getSalary(), 0.00);
-
-
-
-	}
-
-	@Test
-	public void getGenderCountOfEmployee() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		List<EmployeePayRollData> employeePayRollDataList = employeePayRollService.getEmployeeCountByGender();
-		for (EmployeePayRollData data : employeePayRollDataList){
-			System.out.println(data.getGender() + ", " + data.getSalary());
-		}
-		Assert.assertEquals(4, employeePayRollDataList.get(0).getSalary(), 0.00);
-
-	}
-
-	@Test
-	public void addEmployeeToPayroll() throws SQLException {
-
-		EmployeePayRollService employeePayRollService = new EmployeePayRollService();
-		EmployeePayRollData employeePayRollDataList = employeePayRollService.addEmployeeToPayroll("Ajay", 'M', 23000, LocalDate.now());
-		employeePayRollService.getEmployeePayrollDataByName("Ajay");
-
-//		Assert.assertEquals(4, employeePayRollDataList.get(0).getSalary(), 0.00);
-
-	}
 
 }
