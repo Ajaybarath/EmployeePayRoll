07661ad596d13060d0ab8552d598d80e658f2fa4
ajaybarath <ajaybarath3@gmail.com>
2021-09-30 22:16:57 +0530

    ADDED: id and department varible

--- src/main/java/com/bridgeLabz/employeePayroll/Department.java
+++ src/main/java/com/bridgeLabz/employeePayroll/Department.java
@@ -1,2 +1,32 @@
-package com.bridgeLabz.employeePayroll;public class Department {
+package com.bridgeLabz.employeePayroll;
+
+public class Department {
+
+    int id;
+
+    String name;
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Department() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
